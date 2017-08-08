251c6e77249e316a244b30f8fad7551adfc364a6
atan009 <atan009@users.noreply.github.com>
2017-08-08 11:59:34 -0400

    get calories from request

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(window).on("load", function() {
 	var resultMax = "5";
 	var calMin = "cal_min=0";
 	var calMax = "cal_max=50000";
-	var fields = "fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id";
+	var fields = "fields=item_name%2Cbrand_name%2Citem_id%2Cnf_calories";
 	var appID = "appId=c0964a36";
 	var appKey = "appKey=+4d6ceae5b6d8ba87f064bc7e8e2f8589";
 	var url = nutritionixURL + food + "?results=" + resultMin + "%3A" + resultMax + "&" + calMin + "&" + calMax + "&" + fields + "&" + appID + "&" + appKey;
