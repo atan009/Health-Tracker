d354542acfec028754f5e38f59b7cbd78f1aa0cd
atan009 <atan009@users.noreply.github.com>
2017-08-10 10:07:36 -0400

    local storage complete

--- js/app.js
+++ js/app.js
@@ -29,9 +29,12 @@ $(window).on("load", function() {
 		model: foodCont
 	});
 
+
 	var searchList = new foodlist([]);
 	var calItem;
 	var calList = new foodlist([]);
+
+
 
 	searchList.on("add", function(foodItem) {
 		searchItem = searchItem.clone();
@@ -46,6 +49,8 @@ $(window).on("load", function() {
 	});
 
 	calList.on("add", function(foodItem) {
+		searchList.reset();
+		$(".list-search-results").empty();
 		calItem = exItem.clone();
 		$(".food-name", calItem).text(foodItem.get("name"));
 		$(".brand", calItem).text(foodItem.get("brand"));
@@ -56,9 +61,20 @@ $(window).on("load", function() {
 		newCal = newCal + foodItem.get("calories");
 		$("p.total-cal").text("Total: "  + newCal);
 		calItem.appendTo(".cal-list");
+
+		calItem.on('click', function() {
+			cal = $('p.total-cal').text();
+			newCal = parseInt(cal.substring(cal.indexOf(' ') + 1, cal.length));
+			newCal = newCal - foodItem.get("calories");
+			$("p.total-cal").text("Total: " + newCal);
+			calList.remove(foodItem);
+			$(this).remove();
+		});
 	});
 
 	$('.searchButton').on('click', function() {
+		searchList.reset();
+		$(".list-search-results").empty();
 
 		food = $('.searchBox').val();
 		url = nutritionixURL + food + "?results=" + resultMin + "%3A" + resultMax + "&" + calMin + "&" + calMax + "&" + fields + "&" + appID + "&" + appKey;
@@ -67,7 +83,7 @@ $(window).on("load", function() {
 
 		$.getJSON(url, function(data) {
 
-		// console.log(data.hits.length);
+
 		for (var i = 0; i < data.hits.length; i++) {
 			foodItem = new foodCont({name: data.hits[i].fields.item_name, 
 				brand: data.hits[i].fields.brand_name, 
@@ -78,7 +94,7 @@ $(window).on("load", function() {
 			searchList.add(foodItem);
 		}
 
-		// console.log(searchList.models);
+
 
 		}).error(function(e) {
 			alert("Error with nutritionix api call");
@@ -86,6 +102,19 @@ $(window).on("load", function() {
 
 
 	});
-
 	
+
+
+	var retrievedObject = localStorage.getItem('prevCalList');
+
+	window.onbeforeunload = function() {
+		localStorage.setItem('prevCalList', JSON.stringify(calList));
+	}
+	
+	if (retrievedObject.length != []) {
+		retrievedObject = JSON.parse(retrievedObject);
+		for (var i = 0; i < retrievedObject.length; i++) {
+			calList.add(retrievedObject[i]);
+		}
+	}
 });
